28516118f2806aedd40122d6e0bc66903256b5fb
Fang <857559565@qq.com>
2022-07-13 18:01:51 +0800

    实现删除数据脚本

--- src/api/core/integral-grade.js
+++ src/api/core/integral-grade.js
@@ -8,4 +8,10 @@ export default {
       method: "get",
     });
   },
+  removeById(id) {
+    return request({
+      url: `/admin/core/integralGrade/remove/${id}`,
+      method: "delete",
+    });
+  },
 };
