fc7c960e82fe2dab0cac09de79a1fc5ba7a0d314
Fang <857559565@qq.com>
2022-08-10 22:30:07 +0800

    实现嵌套表格的展示

--- src/views/core/dict/list.vue
+++ src/views/core/dict/list.vue
@@ -40,20 +40,46 @@
         <el-button @click="dialogVisible = false"> 取消 </el-button>
       </div>
     </el-dialog>
+    <el-table :data="list" border row-key="id" lazy :load="load">
+      <el-table-column label="名称" align="left" prop="name" />
+      <el-table-column label="编码" prop="dictCode" />
+      <el-table-column label="值" align="left" prop="value" />
+    </el-table>
   </div>
 </template>
 
 <script>
+import dictApi from "@/api/core/dict";
+
 export default {
   // 定义数据
   data() {
     return {
       dialogVisible: false, //文件上传对话框是否显示
       BASE_API: process.env.VUE_APP_BASE_API, //获取后端接口地址
+      list: [], //数据字典列表
     };
   },
 
+  created() {
+    this.fetchData();
+  },
+
   methods: {
+    // 调用api层获取数据库中的数据
+    fetchData() {
+      dictApi.listByParentId(1).then((response) => {
+        this.list = response.data;
+      });
+    },
+
+    //延迟加载子节点
+    getChildren(row, treeNode, resolve) {
+      dictApi.listByParentId(row.id).then((response) => {
+        //负责将子节点数据展示在展开的列表中
+        resolve(response.data);
+      });
+    },
     // 上传多于一个文件时
     fileUploadExceed() {
       this.$message.warning("只能选取一个文件");
@@ -64,6 +90,7 @@ export default {
       if (response.code === 0) {
         this.$message.success("数据导入成功");
         this.dialogVisible = false;
+        this.fetchData();
       } else {
         this.$message.error(response.message);
       }
@@ -77,6 +104,15 @@ export default {
     //Excel数据导出
     exportData() {
       window.location.href = this.BASE_API + "/admin/core/dict/export";
+    },
+    //加载二级节点
+    load(tree, treeNode, resolve) {
+      console.log("tree", tree);
+      console.log("treeNode", treeNode);
+      //获取数据
+      dictApi.listByParentId(tree.id).then((response) => {
+        resolve(response.data);
+      });
     },
   },
 };
